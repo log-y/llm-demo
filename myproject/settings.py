e4443f99f1a0197602ec9cc6957f8f62e3413bbf
logany <logan.yuan@gmail.com>
2024-08-10 17:12:53 -0400

    h

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)b3vo08)@znr564r_y#-nh9s*)l2ej1%c#(6q@w^x^!49utdsc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
